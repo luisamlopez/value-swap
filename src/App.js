a07b3b9ba2437b685583fc3a082aa3655c6b0fb5
Luisa <luisamlopezg1@gmail.com>
2023-11-26 17:14:23 -0400

    Add video background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import CoinSelect from './components/CoinSelect';
+import backgroundVideo from './assets/backgroundVideo.mp4'
+import logo from "./assets/logo.svg"
+
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      <video autoPlay loop muted className='video' >
+        <source src={backgroundVideo} type='video/mp4' />
+      </video>
+      <div className='logo' >
+        <img src={logo} height={"200px"} />
+      </div>
 
+      <div className="container">
+
+        <CoinSelect />
+      </div>
     </div>
   );
 }
---
+++ src/components/CoinSelect.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+const CoinSelect = () => {
+    return (
+        <Box sx={{
+            color: '#fff', zIndex: 1
+        }}>
+            hola
+        </Box>)
+}
+
+export default CoinSelect;
